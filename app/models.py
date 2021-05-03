6e7c88f82d65cd94321e5bab8a8f93a15f728bcd
Dennis Shih <dennisshih89@gmail.com>
2021-05-03 02:14:04 -0700

    register now gives error message if trying to register under existing name.

--- app/models.py
+++ app/models.py
@@ -3,10 +3,10 @@ from app import db
 class User(db.Model):
 
     id=db.Column(db.Integer, primary_key=True)
-    author=db.Column(db.String(70), unique=True, nullable=False)
-
+    name=db.Column(db.String(70), unique=True, index=True, nullable=False)
+    password=db.Column(db.String(70), nullable=False)
     def __repr__(self):
-        return f'<User {self.author}>'
+        return f'<User {self.name}>'
 
 #Task has a title and description
 class Task(db.Model):
@@ -16,7 +16,5 @@ class Task(db.Model):
     description=db.Column(db.String(70), unique=False, nullable=True)
     def __repr__(self):
         return f'<Task {self.title}>'
-
-
 
 db.create_all()
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template, redirect, flash
-
 from app import app
 from app import db
 from app.forms import LoginForm, RegisterForm
 from app.models import User
+
 
 @app.route('/')
 @app.route('/index.html')
@@ -14,8 +14,10 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form=LoginForm()
+
+
     if form.validate_on_submit():
-        '''Check that the user exists,
+        '''Check that the user exists (and that they have a correct password),
          show user error message if account doesnt exist'''
         #flash('Login requested for user {}'.format(
         #form.username.data, form.password.data))
@@ -23,26 +25,52 @@ def login():
         exists = False
 
         users =User.query.all()
-        the_user = None
+        print(users)
+        new_user = None
         for u in users:
-            if u.author == form.author:
-                exists=true
-                the_user=u
+            print(u.name)
+            #reminder to self: form.username.data is the string for name, not form.username
+            if u.name == form.username.data:
+                exists=True
+                new_user=u
                 return redirect('/')
         if not exists:
             flash('User {} does not exist.'.format(
-            form.username.data, form.password.data))
+            form.username.data))
     return render_template('login.html', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form=RegisterForm()
-    #this should create new user if name available then redirect to home page
+
+
+    '''this should create new user if name is available,
+    then redirect to home page'''
     if form.validate_on_submit():
-        flash('Register user {}','Password {}','Confirm password {}'.format(
-        form.username.data, form.password.data, form.confirmpass.data))
-        return redirect('/')
+        #flash('Register user {}','Password {}','Confirm password {}'.format(
+        #form.username.data, form.password.data, form.confirmpass.data))
+        exists = False
+
+        users =User.query.all()
+        new_user = None
+        print(users)
+
+        for u in users:
+            print(u.name)
+            if u.name == form.username.data:
+                exists=True
+                flash('Username {} is taken.'.format(
+                u.name))
+
+                break
+
+        if not exists:
+            new_user=User(name=form.username.data,
+            password=form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect('/')
 
     return render_template('register.html', form=form)
 
@@ -50,3 +78,4 @@ def register():
 @app.route("/logout")
 def logout():
     flash('logout')
+
