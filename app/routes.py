601699e21e68252b832ac04dedfb3aeb34f98c40
Dennis Shih <dennisshih89@gmail.com>
2021-05-02 23:42:30 -0700

    login checks if user exists

--- app/routes.py
+++ app/routes.py
@@ -10,19 +10,42 @@ from app.models import User
 def index():
     return "Home"
 
+#logs the user in when they type in a username and password
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form=LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}'.format(
-        form.username.data))
-        return redirect('/')
+        '''Check that the user exists,
+         show user error message if account doesnt exist'''
+        #flash('Login requested for user {}'.format(
+        #form.username.data, form.password.data))
+
+        exists = False
+
+        users =User.query.all()
+        the_user = None
+        for u in users:
+            if u.author == form.author:
+                exists=true
+                the_user=u
+                return redirect('/')
+        if not exists:
+            flash('User {} does not exist.'.format(
+            form.username.data, form.password.data))
     return render_template('login.html', form=form)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form=RegisterForm()
+    #this should create new user if name available then redirect to home page
+    if form.validate_on_submit():
+        flash('Register user {}','Password {}','Confirm password {}'.format(
+        form.username.data, form.password.data, form.confirmpass.data))
+        return redirect('/')
+
     return render_template('register.html', form=form)
+
 
 @app.route("/logout")
 def logout():
